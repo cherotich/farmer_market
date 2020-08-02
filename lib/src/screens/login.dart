0e97eac907acfe932692a8e99a27311c7819f931
cherotich <carolcaro60@gmail.com>
2020-08-02 21:43:45 +0300

    Cupertino TextFields

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,7 +1,9 @@
+import 'package:farmer_market/src/styles/base.dart';
 import 'package:farmer_market/src/styles/textFields.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import '../styles/colors.dart';
 
 class Login extends StatelessWidget {
   @override
@@ -31,11 +33,13 @@ class Login extends StatelessWidget {
         Container(
           height: 200.0,
           decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/logo.png'))),
+              image:
+                  DecorationImage(image: AssetImage('assets/images/logo.png'))),
         ),
         Padding(
-          padding:  EdgeInsets.symmetric(horizontal: TextFieldStyles.textBoxHorizontal(), vertical:TextFieldStyles.textBoxVertical()),
+          padding: EdgeInsets.symmetric(
+              horizontal: TextFieldStyles.textBoxHorizontal(),
+              vertical: TextFieldStyles.textBoxVertical()),
           child: email(),
         ),
         password(),
@@ -45,18 +49,28 @@ class Login extends StatelessWidget {
 
   Widget email() {
     if (Platform.isIOS) {
-      return CupertinoTextField();
+      return CupertinoTextField(
+        padding: EdgeInsets.all(12.0),
+        placeholder: 'Email',
+        placeholderStyle: TextFieldStyles.placeholder(),
+        style: TextFieldStyles.text(),
+        textAlign: TextFieldStyles.textAlign(),
+        cursorColor: TextFieldStyles.cursorColor(),
+        decoration:TextFieldStyles.cupertinoDecoration(),
+
+   prefix: TextFieldStyles.iconPrefix(CupertinoIcons.mail_solid),
+   
+      );
     } else {
       return TextField();
     }
   }
 
   Widget password() {
-  if (Platform.isIOS) {
+    if (Platform.isIOS) {
       return CupertinoTextField();
     } else {
       return TextField();
     }
-
   }
 }
---
+++ lib/src/styles/base.dart
@@ -0,0 +1,8 @@
+abstract class BaseStyles {
+  static double borderRadius() {
+    return 20.0;
+  }
+   static double borderWidth() {
+    return 2.0;
+  }
+}
